2a96a934d65860e1244d7b45b7ba0fa2acd712eb
PRASENJIT DAS <prasenjitdas@PRASENJITs-MacBook-Air.local>
2024-11-02 09:11:04 +0530

    Project installed and clean

---
+++ app/layout.jsx
@@ -0,0 +1,31 @@
+import localFont from 'next/font/local'
+import './globals.css'
+
+const geistSans = localFont({
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900'
+})
+const geistMono = localFont({
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900'
+})
+
+export const metadata = {
+  title: 'Linksy',
+  description:
+    'Linksy is a next-generation social media platform crafted to foster meaningful connections and enrich user engagement. Developed with Next.js, Clerk, and MongoDB, Linksy emphasizes both user privacy and a tailored experience, allowing users to connect, share, and discover content relevant to their interests within a secure environment.'
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
